507ac26b1b462ad857fcf7ed89775570852b6b59
Pandora <nimfanefor@gmail.com>
2023-08-22 14:00:15 +0600

    Дописал урлы

--- source/api_v2/urls.py
+++ source/api_v2/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from api_v2.views import get_csrf_token
-from api_v2.views import ArticleView
+from api_v2.views import ArticleView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 app_name = "api_v2"
 
 urlpatterns = [
     path("articles/", ArticleView.as_view(), name="articles"),
-    path("articles/<int:pk>/", ArticleView.as_view(), name="article"),
-    path("get-csrf-token/", get_csrf_token)
+    # path("articles/<int:pk>/", ArticleView.as_view(), name="article"),
+    path("get-csrf-token/", get_csrf_token),
+    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('articles/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+
 ]
